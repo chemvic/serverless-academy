c1035b78ca82654c0da3d735051b3f1f96b90669
chemvic <chemvic79@gmail.com>
2023-10-26 19:59:33 +0300

    clean

--- 03_cli_telegram_console_sender/setenv.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const env = fs.readFileSync('.env', 'utf-8').split('\n');
-env.forEach((line) => {
-  const [key, value] = line.split('=');
-  process.env[key] = value;
-});
-
