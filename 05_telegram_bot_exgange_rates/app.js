bac40adb208d34847b68d1cd16a777ae17d28f1b
chemvic <chemvic79@gmail.com>
2023-10-30 10:57:57 +0200

    кеширование данных от монобанка

--- 05_telegram_bot_exgange_rates/app.js
+++ 05_telegram_bot_exgange_rates/app.js
@@ -52,53 +52,54 @@ try {
 
           const responsePrb = await axios(PRB_URL);
         //   let responseMnb = await axios(MNB_URL);
-
-        //   let success = myCache.mset([
-        //     {key: "buy$", val: responseMnb.data[0].rateBuy, ttl: 10000},
-        //     {key: "sale$", val: responseMnb.data[0].rateSell},
-        //     {key: "buy€", val: responseMnb.data[1].rateBuy},
-        //     {key: "sale€", val: responseMnb.data[1].rateSell},
-        // ]) 
-
-console.log(`Privatbank:\n Buy $: ${responsePrb.data[1].buy} uah   Sale $: ${responsePrb.data[1].sale} uah\n 
- Buy €: ${responsePrb.data[0].buy} uah   Sale €: ${responsePrb.data[0].sale} uah\n\n`);
-// console.log(`Monobank:\n Buy $: ${responseMnb.data[0].rateBuy} uah   Sale $: ${responseMnb.data[0].rateSell} uah\n 
-//  Buy €: ${responseMnb.data[1].rateBuy} uah   Sale €: ${responseMnb.data[1].rateSell} uah\n\n`);
-
 let message='';
+
 if (currency === 'USD') {
+
     let value = myCache.mget( [ "buyUSD", "saleUSD" ] );
-    if (value === undefined) {
+
+    if (value['buyUSD'] === undefined ) {
+
        let responseMnb = await axios(MNB_URL);
+       console.log(`Monobank:\n Buy $: ${responseMnb.data[0].rateBuy} uah   Sale $: ${responseMnb.data[0].rateSell} uah\n 
+       Buy €: ${responseMnb.data[1].rateBuy} uah   Sale €: ${responseMnb.data[1].rateSell} uah\n\n`);
         myCache.mset([
              {key: "buyUSD", val: responseMnb.data[0].rateBuy, ttl: 300},
              {key: "saleUSD", val: responseMnb.data[0].rateSell},
              {key: "buyEUR", val: responseMnb.data[1].rateBuy},
              {key: "saleEUR", val: responseMnb.data[1].rateSell},
          ]);
-        value = myCache.mget( [ "buyUSD", "saleUSD" ] );
+        
     };
-
+value = myCache.mget( [ "buyUSD", "saleUSD" ] );
     message = ` Privatbank:\n Buy $: ${responsePrb.data[1].buy} uah\n Sale $: ${responsePrb.data[1].sale} uah\n\n Monobank:\n Buy $: ${value['buyUSD']} uah\n Sale $: ${value['saleUSD']} uah\n\n`;
     bot.sendMessage(msg.chat.id, message);
+   
 };
  if (currency==='EUR') {
-    let value = myCache.mget( [ "buy€", "sale€" ] );
-    if (value===undefined) {
+    let value = myCache.mget( [ "buyEUR", "saleEUR" ] );
+    
+    if (value['buyEUR']===undefined) {
        let responseMnb = await axios(MNB_URL);
         myCache.mset([
-             {key: "buy$", val: responseMnb.data[0].rateBuy, ttl: 300},
-             {key: "sale$", val: responseMnb.data[0].rateSell},
-             {key: "buy€", val: responseMnb.data[1].rateBuy},
-             {key: "sale€", val: responseMnb.data[1].rateSell},
+            {key: "buyUSD", val: responseMnb.data[0].rateBuy, ttl: 300},
+            {key: "saleUSD", val: responseMnb.data[0].rateSell},
+            {key: "buyEUR", val: responseMnb.data[1].rateBuy},
+            {key: "saleEUR", val: responseMnb.data[1].rateSell},
          ]);
-        value = myCache.mget( [ "buy€", "sale€" ] );
+       console.log(`After  Monobank:\n Buy $: ${responseMnb.data[0].rateBuy} uah   Sale $: ${responseMnb.data[0].rateSell} uah\n 
+          Buy €: ${responseMnb.data[1].rateBuy} uah   Sale €: ${responseMnb.data[1].rateSell} uah\n\n`); 
+
+
     };
-  
-    message=` Privatbank:\n Buy €: ${responsePrb.data[0].buy} uah\n Sale €: ${responsePrb.data[0].sale} uah\n\n Monobank:\n Buy €: ${value['buy€']} uah\n Sale €: ${value['sale€']} uah\n\n`;
+   value = myCache.mget( [ "buyEUR", "saleEUR" ] );
+    message=` Privatbank:\n Buy €: ${responsePrb.data[0].buy} uah\n Sale €: ${responsePrb.data[0].sale} uah\n\n Monobank:\n Buy €: ${value['buyEUR']} uah\n Sale €: ${value['saleEUR']} uah\n\n`;
           bot.sendMessage(msg.chat.id, message);
+
          
  };
+console.log(`Privatbank:\n Buy $: ${responsePrb.data[1].buy} uah   Sale $: ${responsePrb.data[1].sale} uah\n 
+ Buy €: ${responsePrb.data[0].buy} uah   Sale €: ${responsePrb.data[0].sale} uah\n\n`);
 
 } catch (error) {
   
