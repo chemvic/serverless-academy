f6815d5a320fa48d4dcc9fa19da4a7932d18e65c
chemvic <chemvic79@gmail.com>
2023-11-01 13:16:56 +0200

    Update app.js

--- 06_instagram_giveaway/app.js
+++ 06_instagram_giveaway/app.js
@@ -1,18 +1,15 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-function uniqueNames(allNames){
-console.log("start of unique");
-  const uniqueNames = allNames.filter(
-    (name, index, array) => array.indexOf(name) === index);
-  console.log(uniqueNames);
-  console.log(uniqueNames.length);  
-};
-
-
 const folderPath = path.join(__dirname, 'files');
-``
+
+
+let startTime;
+let finishTime;
+
 async function grabAllFiles() {
+    startTime=Date.now();
+
   const filesArrays = {};
    const files = await fs.readdir(folderPath);
   try {    
@@ -22,29 +19,88 @@ async function grabAllFiles() {
     const data = await fs.readFile(filePath, 'utf8');
     filesArrays[file] = data.split('\n');
   };
-  let allNames = [].concat(...Object.values(filesArrays));
-  return allNames;
+  
+  return filesArrays;
   
   } catch (error) {
     console.error('Ошибка при чтении файла outN:', error);
   };
 };
-grabAllFiles().then((allNames) => uniqueNames(allNames));
+
+
+
+async function uniqueNames(){
+    const filesArrays = await grabAllFiles();
+
+    let allNames = [].concat(...Object.values(filesArrays));
+  
+    const uniqueNames = [...new Set(allNames)];
+
+    finishTime=Date.now();
+    const elapsedTime = (finishTime-startTime)/1000;  
+
+    // console.log(uniqueNames);
+    console.log(uniqueNames.length);  
+    console.log(`elapsedTime: ${elapsedTime} s`);
+    return uniqueNames.length;
+  };
+
+
+
+async function existInAllFiles() {
+    const filesArrays = await grabAllFiles();
+   
+    let nameCounts = {};
+  
+    for (let names of Object.values(filesArrays)) {
+
+        for (let name of [...new Set(names)]){
+        if (!nameCounts[name]){
+         nameCounts[name] = 0;             
+        };
+         nameCounts[name]+=1;
+      } 
+    }
+
+  const quantityOfMatches = Object.values(nameCounts).filter(count => count === Object.keys(filesArrays).length).length;
+    finishTime=Date.now();
+    const elapsedTime = (finishTime-startTime)/1000;    
+
+    console.log(quantityOfMatches);
+    console.log(`elapsedTime: ${elapsedTime} s`);
+    return quantityOfMatches;
+  };
 
 
 
 
-// async function readFile(){
-//  const outFilePath = path.join(__dirname, 'files', 'out0.txt');
-// try {
-//     const data = await fs.readFile(outFilePath, 'utf8');
-//     const names = data.split('\n');
-    
-//     console.log(names);
-//     return names;
-// } catch (error) {
-//     console.error('Ошибка при чтении файла outN:', error);
-// }   
-// };
 
-// // readFile();
+async function existInAtleastTen() {  
+    const filesArrays = await grabAllFiles(); 
+
+    let nameCounts = {};
+  
+    for (let names of Object.values(filesArrays)) {
+
+        for (let name of [...new Set(names)]){
+        if (!nameCounts[name]){
+         nameCounts[name] = 0;             
+        };
+         nameCounts[name]+=1;
+      } 
+    }
+
+  const quantityOfMatcheInAtleastTen = Object.values(nameCounts).filter(count => count >=10).length;
+    finishTime=Date.now();
+    const elapsedTime = (finishTime-startTime)/1000;    
+
+    console.log(quantityOfMatcheInAtleastTen);
+    console.log(`elapsedTime: ${elapsedTime} s`);
+    return quantityOfMatcheInAtleastTen;
+  };
+
+
+uniqueNames();
+existInAllFiles();
+existInAtleastTen();
+
