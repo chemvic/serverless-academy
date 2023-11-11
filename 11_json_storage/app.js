3cfdb06f4760a0328585d65bc7b7905655e9efa5
chemvic <chemvic79@gmail.com>
2023-11-11 14:48:19 +0200

    routes

--- 11_json_storage/app.js
+++ 11_json_storage/app.js
@@ -4,9 +4,40 @@ const app= express();
 
 app.use(express.json());
 
+const jsonBase = {};
 
-app.put('/put',);
-app.get('/get',);
+
+
+const putToBase=(req, res)=>{
+    const json_name =req.params.json_name; 
+    const jsonPayload =req.body;
+
+    jsonBase[json_name]=jsonPayload;
+
+    console.log(json_name);
+    console.log(jsonBase);
+    res.status(201).json({
+        "json_name": json_name,
+ "jsonBase": jsonBase});
+};
+
+const getFromBase =(req, res)=>{
+    const json_name =req.params.json_name;
+    let jsonPayload;
+    if (jsonBase[json_name]) {
+        jsonPayload =jsonBase[json_name];
+        console.log(jsonPayload);
+    } else{
+        res.status(404).json({message: "Not found"})
+    }
+    res.status(200).json({
+        "json_name": json_name,
+ "jsonPayload": jsonPayload});
+}
+
+
+app.put('/data/put/:json_name', putToBase);
+app.get('/data/get/:json_name', getFromBase);
 
 
 app.listen(3000, ()=>{
